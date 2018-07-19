f8f80724c5a2fea82c7f1d18e59acbb3efb148b1
Dane Roe <daneroe@gmail.com>
2018-07-19 12:54:50 +0930

    Initial commit

---
+++ api/controllers/CustomerController.js
@@ -0,0 +1,16 @@
+/**
+ * CustomerController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    'new': function (req, res) {
+        res.view();
+    }
+
+
+};
+
